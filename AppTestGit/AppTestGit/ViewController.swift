f70a1c49cd420c6eb6653615ff6634845cbdbd68
Victor Monteiro <victor.monteiro21@icloud.com>
2021-11-20 10:28:01 -0700

    adding files

--- AppTestGit/AppTestGit/ViewController.swift
+++ AppTestGit/AppTestGit/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     let name = "Victor"
+    let error = "Error Description"
 
     override func viewDidLoad() {
         super.viewDidLoad()
